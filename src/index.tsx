0a91758392a0a0a2265f2de9f70092c25b1d9c26
Vyacheslav Osepyan <mr-93@mail.ru>
2023-12-01 19:19:56 +0300

    Organized the project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import 'resources/styles/index.css';
+import App from './main/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
