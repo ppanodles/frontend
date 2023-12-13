4e445cb7f5258411039e02de67550e902e0ee629
Emil Absalyamov <absalyamov.work@gmail.com>
2023-12-14 02:25:42 +0400

    Refactor `monitoringStations` mapper for new data format

--- src/library/mappers/monitoringStations.mapper.ts
+++ src/library/mappers/monitoringStations.mapper.ts
@@ -4,14 +4,14 @@ import { IMonitoringStation } from 'library/types/marineFarming.d';
 
 export default (data: any): IMonitoringStation[] => data?.map((datum: any) => ({
 	id: uuid(),
-	name: datum?.name ?? '',
-	long: datum?.long ?? 0,
-	lat: datum?.lat ?? 0,
-	metals: datum?.metals ? datum?.metals.map((metal: any) => ({
+	name: datum?.station ?? '',
+	long: datum?.coordinates[1] ?? 0,
+	lat: datum?.coordinates[0] ?? 0,
+	metals: datum?.indications ? datum?.indications.map((metal: any) => ({
 		name: metal?.name ?? '',
-		label: metal?.label ?? '',
-		underworld: metal?.underworld ?? 0,
-		surface: metal?.surface ?? 0,
-		leap: metal?.leap ?? 0,
+		label: metal?.abbreviation ?? '',
+		underworld: Number(metal?.data?.bottom) ?? 0,
+		surface: Number(metal?.data?.surface) ?? 0,
+		leap: Number(metal?.data?.jump) ?? 0,
 	})) : [],
 })) ?? [];
